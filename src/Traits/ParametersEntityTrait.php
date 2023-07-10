5f4eb7c574b7e9228b8b8165898678a233e0787b
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-07-10 17:48:27 +0200

    reprise logique gestion liste parametres

--- src/Traits/ParametersEntityTrait.php
+++ src/Traits/ParametersEntityTrait.php
@@ -2,13 +2,72 @@
 
 namespace Celtic34fr\ContactCore\Traits;
 
+use DateTimeImmutable;
 use Celtic34fr\ContactCore\Entity\Parameter;
 
 trait ParametersEntityTrait
 {
+    public function getParam(): Parameter
+    {
+        return $this->param;
+    }
+
+    /**
+     * Set the value of param
+     * @return  self
+     */
+    public function setParam(Parameter $param)
+    {
+        $this->param = $param;
+        return $this;
+    }
+
+    public function getId(): mixed
+    {
+        return $this->param->getId();
+    }
+
+    public function getOrd(): mixed
+    {
+        return $this->param->getOrd() ?? false;
+    }
+
+    /**
+     * Set the ord of param
+     * @return  self
+     */
+    public function setOrd(int $ord)
+    {
+        $this->param->setOrd($ord);
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     */
+    public function getUpdatedAt(): DateTimeImmutable
+    {
+        return $this->param->getUpdatedAt();
+    }
+
+    public function isEmptyUpdatedAt()
+    {
+        return $this->param->isEmptyUpdatedAt();
+    }
+
+    /**
+     * Set the value of updated_at
+     * @return  self
+     */
+    public function setUpdatedAt(DateTimeImmutable $updated_at): self
+    {
+        $this->param->setUpdatedAt($updated_at);
+        return $this;
+    }
+
     public function getValues(): array
     {
-        $datas = $this->valeur;
+        $datas = $this->param->getValeur();
         if ($datas) {
             $datas = explode("|", $datas);
             return array_combine(self::HEADER, $datas);
@@ -19,7 +78,7 @@ trait ParametersEntityTrait
     public function setValues(array $values)
     {
         $chaine = implode('|', $values);
-        return $this->valeur = $chaine;
+        return $this->param->setValeur($chaine);
     }
 
     public function getItem(string $key): mixed
@@ -49,14 +108,5 @@ trait ParametersEntityTrait
             $natures[] = $item['name'];
         }
         return $natures;
-    }
-
-    public function persist($entity): void
-    {
-        $parameter = new Parameter();
-        $parameter->setCle($entity->getCle());
-        $parameter->setOrd($entity->getOrd());
-        $parameter->setValeur($entity->getValeur());
-        $this->em->flush($parameter);
     }
 }
