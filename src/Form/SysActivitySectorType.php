7012182697352349bb24c192f2d6de3f5558887c
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2024-05-25 18:33:35 +0200

    correction missing elements

--- src/Form/SysActivitySectorType.php
+++ src/Form/SysActivitySectorType.php
@@ -2,11 +2,13 @@
 
 namespace Celtic34fr\ContactCore\Form;
 
+use Celtic34fr\ContactCore\FormEntity\SysActivitySector;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
 
@@ -34,5 +36,12 @@ class SysActivitySectorType extends AbstractType
             'label' => 'Enregistrer',
         ])
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SysActivitySector::class,
+        ]);
     }
 }
